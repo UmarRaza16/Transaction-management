a499e13d41f635df74e4878fc5bf393fde13b7e5
Md Umar Raza <114905080+UmarRaza16@users.noreply.github.com>
2026-02-05 20:34:45 +0530

    Change login API URL to production
    Updated login API URL to production endpoint.

--- LoginPanel/frontend/src/pages/Login.js
+++ LoginPanel/frontend/src/pages/Login.js
@@ -27,7 +27,7 @@ function Login() {
             return handleError('email and password are required')
         }
         try {
-            const url =  "http://localhost:8080/auth/login";
+            const url =  `https://transaction-management-sigma.vercel.app/auth/login`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
@@ -89,5 +89,6 @@ function Login() {
         </div>
     )
 }
+
 
 export default Login
