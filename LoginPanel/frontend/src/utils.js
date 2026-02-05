05ce1b000def6825e6c56e649d23e73f06b704a0
Md Umar Raza <114905080+UmarRaza16@users.noreply.github.com>
2026-02-05 20:51:52 +0530

    Update utils.js

--- LoginPanel/frontend/src/utils.js
+++ LoginPanel/frontend/src/utils.js
@@ -12,4 +12,5 @@ export const handleError = (msg) => {
     })
 }
 
-export const APIUrl = process.env.REACT_APP_URL || 'http://localhost:8080';
+
+export const APIUrl = https://transaction-management-sigma.vercel.app || 'http://localhost:8080';
